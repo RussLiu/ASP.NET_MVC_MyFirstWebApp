d86c3f844cfc0c657a495c2022a065ee5880de0a
Russ <yaohua@synergy-tech.com.tw>
2024-07-03 17:25:22 +0800

    Day7 Finish

--- MyFirstWebApp/MyFirstWebApp/Controllers/DemoController.cs
+++ MyFirstWebApp/MyFirstWebApp/Controllers/DemoController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MyFirstWebApp.Models;
 
 namespace MyFirstWebApp.Controllers
 {
@@ -25,6 +26,31 @@ namespace MyFirstWebApp.Controllers
         public string ShowPrice(string product, int price)
         {
             return $"商品是:{product}，價格是{price}";
+        }
+
+        public ActionResult ShowMembers()
+        {
+            var memberA = new Member() { Id = 1, Name = "Alex", Age = 12 };
+            var memberB = new Member() { Id = 2, Name = "Ben", Age = 34 };
+            var memberC = new Member() { Id = 3, Name = "Carol", Age = 56 };
+            var memberList = new List<Member>() { memberA, memberB, memberC };
+
+            return View(memberList);
+        }
+
+        public ActionResult ShowShoppingList()
+        {
+            var member = new Member() { Id = 1, Name = "Alex", Age = 12 };
+            var products = new List<Product>();
+            products.Add(new Product { Number = 1, Name = "Apple", Price = 30 });
+            products.Add(new Product { Number = 2, Name = "Banana", Price = 50 });
+            products.Add(new Product { Number = 3, Name = "Cherry", Price = 100 });
+
+            var memberInfo = new MemberInfoViewModel();
+            memberInfo.Member = member;
+            memberInfo.Products = products;
+
+            return View(memberInfo);
         }
     }
 }
